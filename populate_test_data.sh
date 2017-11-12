e8104c1d688515964bda6121de3e031bf0323c3a
Danilo Pereira <dnp1@users.noreply.github.com>
2017-11-12 03:37:45 -0200

    Fix in populate script

--- populate_test_data.sh
+++ populate_test_data.sh
@@ -4,11 +4,5 @@ export PGPASSWORD="mysecretpassword"
 export PGHOST="localhost"
 export PGUSER=postgres
 
-dropdb file
-dropdb user
-dropdb article
-createdb file
-createdb article
-createdb user
 psql -f src/test_data/article.sql  article
 psql -f src/test_data/user.sql  user
--- src/test_data/user.sql
+++ src/test_data/user.sql
@@ -1,5 +1,5 @@
 INSERT INTO "session" DEFAULT VALUES ;
 SELECT create_user(1, 'd@danilo.info', 'password*$#@', 'Danilo', 'Pereira');
 SELECT create_user(1, 'viviane@danilo.info', 'senha123@#$%', 'Viviane', 'Almeida');
-UPDATE "user" SET "user".external_id = 'f47701f7-cabc-4db2-b2fc-053c9137c447' WHERE id =1;
-UPDATE "user" SET "user".external_id = '7f9eac7e-f07c-4556-9539-c95ceab5c97f' WHERE id =2;
+UPDATE "user" SET external_id = 'f47701f7-cabc-4db2-b2fc-053c9137c447' WHERE id =1;
+UPDATE "user" SET external_id = '7f9eac7e-f07c-4556-9539-c95ceab5c97f' WHERE id =2;
