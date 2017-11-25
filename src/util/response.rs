1e4e806f90fbb6d7ae2ee2870b84ad08248dd793
Danilo Pereira <dnp1@users.noreply.github.com>
2017-11-25 16:18:07 -0200

    JSON response module reorganization

--- src/util/response.rs
+++ src/util/response.rs
@@ -9,17 +9,21 @@ use iron::modifier::Set;
 
 pub struct Json<T : Serialize> (pub T);
 
+#[inline]
+fn get_json_mime() -> mime::Mime {
+    "application/json".parse().unwrap()
+}
+
 impl <T> Modifier<Response> for Json<T> where T:Serialize {
     fn modify(self, res: &mut Response) {
-
+        res.set_mut(get_json_mime());
         match serde_json::to_vec(&self.0) {
             Err(err) => {
                 res.status = Some(status::InternalServerError);
-                res.body = Some(Box::new(""));
+                let b = r#"{"code": "E01", "message": "Data serialization has failed"}"#;
+                res.body = Some(Box::new(b));
             }
             Ok(value) => {
-                let json_mime: mime::Mime = "application/json".parse().unwrap();
-                res.set_mut(json_mime);
                 res.body = Some(Box::new(value));
             }
         }
